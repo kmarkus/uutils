2355e1fc6765d6fc97369fb8558b4eb7d7ddb6ec
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2013-10-21 20:51:59 +0200

    bump version 0.995

--- utils.lua
+++ utils.lua
@@ -12,7 +12,7 @@ module('utils')
 
 -- increment major on API breaks
 -- increment minor on non breaking changes
-VERSION=0.994
+VERSION=0.995
 
 function append(car, ...)
    assert(type(car) == 'table')
