bdedd045babff1b013f0c6f5fc17a9b2cdd7b8e7
Markus Klotzbuecher <mk@mkio.de>
2020-11-26 19:49:53 +0100

    time: minor update
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- lua/time.lua
+++ lua/time.lua
@@ -1,31 +1,12 @@
---
--- (C) 2010-2017 Markus Klotzbuecher, mk@marumbi.de
---
--- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
--- furnished to do so, subject to the following conditions:
---
--- The above copyright notice and this permission notice shall be included in
--- all copies or substantial portions of the Software.
---
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
--- THE SOFTWARE.
-
 --- Various sys/time.h like operations.
+--
+-- (C) 2010-2013 Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
+-- (C) 2014-2020 Markus Klotzbuecher <mk@mkio.de>
+--
+-- SPDX-License-Identifier: BSD-3-Clause
+--
 -- Take struct timespec tables with 'sec' and 'nsec' fields as input
 -- and return two values sec, nsec
--- @release Released under MIT
--- @copyright Markus Klotzbuecher, Katholieke Universiteit Leuven, Belgium.
-
-
 
 local M = {}
 
